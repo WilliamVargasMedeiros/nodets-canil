f28df323f894b006b61c2d64dea47747e37a39d6
william.vargas <william.vargas@freedom.net.br>
2023-04-19 20:48:07 -0300

    Implementação

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,17 +1,39 @@
 import { Request, Response } from 'express';
+import { createMenuObject } from '../helpers/createMenuObject';
 
 export const home = (req: Request, res: Response) => {
-    res.render('pages/page');
+    res.render('pages/page', {
+        menu: createMenuObject('all'),
+        banner: {
+            title: 'Todos os animais',
+            background: 'allanimals.jpg'
+        }
+    });
 }
 export const dogs = (req: Request, res: Response) => {
-    res.send('dogs');
-    res.render('pages/page');
+    res.render('pages/page', {
+        menu: createMenuObject('dog'),
+        banner: {
+            title: 'Cachorros',
+            background: 'banner_dog.jpg'
+        }
+    });
 }
 export const cats = (req: Request, res: Response) => {
-    res.send('cats');
-    //res.render('pages/page');
+    res.render('pages/page', {
+        menu: createMenuObject('cat'),
+        banner: {
+            title: 'Gatos',
+            background: 'banner_cat.jpg'
+        }
+    });
 }
 export const fishes = (req: Request, res: Response) => {
-    res.send('fishes');
-    //res.render('pages/page');
+    res.render('pages/page', {
+        menu: createMenuObject('fish'),
+        banner: {
+            title: 'Peixes',
+            background: 'banner_fish.jpg'
+        }
+    });
 }
